fd61bed25f8453f3d482e9a1ba26b12f911986d3
Katie <kbrad12@uw.edu>
2022-11-14 12:39:17 -0800

    Putting the source files in their correct place

---
+++ source/tablesummary.R
@@ -0,0 +1,55 @@
+unemployment_rate_women <- read.csv("C:/Users/katie/Documents/info201/assignments/project-group-3-section-ad/data/unemployment-rate-women.csv")
+View(unemployment_rate_women)
+
+names(unemployment_rate_women) [4] = "Unemployment_rate_women"
+
+unemployment_rate_women$Unemployment_rate_women <- round(unemployment_rate_women$Unemployment_rate_women, digit=2)
+
+long_run_perspective <- read.csv("C:/Users/katie/Documents/info201/assignments/project-group-3-section-ad/data/Long run perspective on female labor force participation rates.csv")
+View(long_run_perspective)
+
+names(long_run_perspective) [4] = "long_run_perspective_female_labor_force_participation_rate"
+
+female_labor_force_by_age <- read.csv("C:/Users/katie/Documents/info201/assignments/project-group-3-section-ad/data/female-labor-force-by-age.csv")
+View(female_labor_force_by_age)
+
+names(female_labor_force_by_age) [4] = "65+_females_in_labor_force"
+names(female_labor_force_by_age) [5] = "55-64_females_in_labor_force"
+names(female_labor_force_by_age) [6] = "45-54_females_in_labor_force"
+names(female_labor_force_by_age) [7] = "35-44_females_in_labor_force"
+names(female_labor_force_by_age) [8] = "25-34_females_in_labor_force"
+names(female_labor_force_by_age) [9] = "15-24_females_in_labor_force"
+
+ratio_of_female_to_labor_force_participation_rates <- read.csv("C:/Users/katie/Documents/info201/assignments/project-group-3-section-ad/data/ratio-of-female-to-male-labor-force-participation-rates.csv")
+View(ratio_of_female_to_labor_force_participation_rates)
+
+names(ratio_of_female_to_labor_force_participation_rates) [4] = "ratio_of_female_to_male_labor_force_participation_rate"
+
+ratio_of_female_to_labor_force_participation_rates$ratio_of_female_to_male_labor_force_participation_rate <- round(ratio_of_female_to_labor_force_participation_rates$ratio_of_female_to_male_labor_force_participation_rate, digit=2)
+
+summary_table_1 <- left_join(unemployment_rate_women, long_run_perspective, group_by= c("Year", "Entity", "Code"), all.x=TRUE)
+View(summary_table_1)
+  
+summary_table_2 <- left_join(summary_table_1, female_labor_force_by_age, group_by = c("Year", "Entity", "Code"), all.x=TRUE)
+View(summary_table_2)
+
+summary_table_3 <- left_join(summary_table_2, ratio_of_female_to_labor_force_participation_rates, group_by=c("Year", "Entity", "Code"), all.x=TRUE)
+
+names(summary_table_3) [1] = "Country"
+
+View(summary_table_3)
+
+?group_by
+
+?groupby
+
+
+
+?left_join
+
+
+totals_by_day <- left_join(county_by_day, state_by_day, by = "date")
+View(totals_by_day)
+
+library(dplyr)
+library(tidyverse)
