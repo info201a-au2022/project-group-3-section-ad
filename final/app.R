0eb781e8eb7cec94cf9d496ab6d3d52047e75347
jbkauff <jbkauff@uw.edu>
2022-12-02 10:58:36 -0800

    These are the changes for review by TA

--- final/app.R
+++ final/app.R
@@ -1,18 +1,64 @@
-#
-# This is a Shiny web application. You can run the application by clicking
-# the 'Run App' button above.
-#
-# Find out more about building applications with Shiny here:
-#
-#    http://shiny.rstudio.com/
-#
-
+library(ggplot2)
+library(plotly)
 library(shiny)
 library(dplyr)
+library(tidyverse)
+library(stringr)
+library(shiny)
+library(ggthemes)
 
-source("app_ui.R")
-source("app_server.R")
+# Dataset
+chart_3_data_set_complete <- read.csv("C:/Users/jbkka/Documents/info201/assignments/project-group-3-section-ad/data/female-labor-force-by-age-chart-3-v2.csv")
+
+year_range <- range(chart_3_data_set_complete$Year)
+
+year_slider <- sliderInput(inputId = "Year",
+                           label = "Year Range", sep = "",
+                           min = min(chart_3_data_set_complete$Year),
+                           max = max(chart_3_data_set_complete$Year),
+                           value = c(1990,2030))
+
+# Home Page
+intro_tab <- tabPanel(
+  # Title
+  "Introduction",
+  fluidPage(
+    h4(strong(em("Background Information"))),
+    p("Emissions have become increasingly top-of-mind for countries around the world as climate change
+      and negative externalities of our actions have come to light. Using the data of over 207 country profiles
+      provided by Our World in Data, it allows the user to explore climate-related statistics for every country 
+      in the world spanning over a century. Within this comprehensive data set, I have chosen to focus on a few 
+      variables in particular pertaining to CO2 emissions and their differing relationships to countries both large
+      and small, developed and developing. With this in mind, we will be looking at the correlation of 'co2_per_capita'
+      and 'share_global_co2' over the period in which the data was collected and reported."),
+    p("Like previously mentioned, this data set was initially published in May of 2017 by Our World in Data with the 
+      help of Hannah Ritchie, Max Roser, and Pablo Rosado."),
+  )
+)
+# Ui
+ui <- fluidPage(h1(strong("Female Labor Force (Group 7 Section AD)"),
+                   style = "color : Red"),
+                tabsetPanel(
+                  tabPanel("Introduction", intro_tab),
+                  tabPanel("Female Labor Force by Age",
+                           fluidRow(
+                             column(year_slider, width = 6)),
+                           fluidRow(
+                             column(plotlyOutput("chart3"), width = 12)),
+                           p("This will be caption for the stacked area chart.")
+                  )))
+
+# Server
+server <- function(input, output) {
+  
+  output$chart3 <- renderPlotly({
+    ggplotly(
+      ggplot(chart_3_data_set_complete, aes(x = Year, y = Number, fill = Age)) +
+        geom_area(colour = "black", size = .2, alpha = .4) +
+        scale_fill_brewer(palette = "Reds") +
+        geom_line(position = "stack", size = .3) )})}
 
 
-# Run the application 
+# Run App
 shinyApp(ui = ui, server = server)
+
--- source/Chart3.R
+++ source/Chart3.R
@@ -3,7 +3,7 @@ library(ggplot2)
 library(dplyr)
 
 #Stacked Area Chart
-chart_3_data_set_complete <- read.csv("C:/Users/katie/Documents/info201/assignments/project-group-3-section-ad/data/female-labor-force-by-age-chart-3-v2.csv")
+chart_3_data_set_complete <- read.csv("../../project-group-3-section-ad/data/female-labor-force-by-age-chart-3-v2.csv")
 View(chart_3_data_set_complete)
 
 chart3 <- ggplot(chart_3_data_set_complete, aes(x = Year, y = Number, fill = Age)) +
@@ -17,6 +17,7 @@ chart3 <- ggplot(chart_3_data_set_complete, aes(x = Year, y = Number, fill = Age
         panel.grid.minor = element_blank(),
         panel.background = element_blank()) 
 
+chart3
 # Summary Paragraph
 # We chose to incorporate this graph into our research as we believe it does 
 # a great job of showing the changes in the number of females in the labor
@@ -26,3 +27,29 @@ chart3 <- ggplot(chart_3_data_set_complete, aes(x = Year, y = Number, fill = Age
 # One immediate observation from the chart is that groups, particularly ages
 # 15-24, 25-34, and 35-44 have experienced the highest growth rates over the 
 # period.
+
+# Data at the end of the ui function
+sidebarLayout(
+  sidebarPanel(),
+  mainPanel(
+    plotlyOutput("chart3"))))
+
+# Server Function Correct One
+visualization <- plot_ly(plot_data,
+                         x = ~Year,
+                         y = ~Number,
+                         type = "scatter",
+                         mode = "none",
+                         stackgroup = "one",
+                         color = "Age",
+                         text = ~paste(plot_data$Year,"<b>,<b>",plot_data$Number,"<br>"),
+                         hoverinfo = "text") %>% 
+  
+  layout(title = "Female Labor Force by Age",
+         xaxis = list(title = "Year"),
+         yaxis = list(title = "Number"))
+
+return(visualization)
+
+})
+}
